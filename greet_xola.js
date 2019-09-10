f140ee605ba503f209480bc451f18cd94faab09a
Masibulele22 <sticky.ngondo@gmail.com>
2019-09-10 15:13:39 +0200

    first commit

--- greet_xola.js
+++ greet_xola.js
@@ -3,5 +3,4 @@ const chalk = require('chalk');
 const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
-
+console.log(styledMessage);
